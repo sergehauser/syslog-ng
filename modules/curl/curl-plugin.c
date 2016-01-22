fbd5717b5184dd614473bc1297781985a2f298cb
Marc Falzon <marc@falzon.me>
2016-01-22 13:31:44 +0100

    Add "curl" module template code

---
+++ modules/curl/curl-plugin.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Marc Falzon
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "cfg-parser.h"
+#include "plugin.h"
+#include "plugin-types.h"
+
+extern CfgParser curl_parser;
+
+static Plugin curl_plugins[] =
+{
+  {
+    .type = LL_CONTEXT_DESTINATION,
+    .name = "curl",
+    .parser = &curl_parser,
+  },
+};
+
+gboolean
+curl_module_init(GlobalConfig *cfg, CfgArgs *args)
+{
+  plugin_register(cfg, curl_plugins, G_N_ELEMENTS(curl_plugins));
+  return TRUE;
+}
+
+const ModuleInfo module_info =
+{
+  .canonical_name = "curl",
+  .version = VERSION,
+  .description = "Please fill this description",
+  .core_revision = SOURCE_REVISION,
+  .plugins = curl_plugins,
+  .plugins_len = G_N_ELEMENTS(curl_plugins),
+};
+
